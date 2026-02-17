4f9c8808aec0ca25a7863aab155dc419c0266e16
Pilar <pilaralvarez080@gmail.com>
2026-02-17 19:34:08 -0300

    El primer commit

---
+++ holagit.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Hello Git</title>
+</head>
+<body>
+    <h1>Hola Git</h1>
+    <p>Este es un archivo simple de PHP para demostrar el control de versiones de Git.</p>
+</body>
+</html>
