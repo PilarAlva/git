749bfe0ad2329d34694b6c26f66bfe114bbba9e3
Pilar <pilaralvarez080@gmail.com>
2026-02-18 12:57:31 -0300

    Sobre rama  main. Nuevo contenido en vuelvegit.php

--- vuelvegit.php
+++ vuelvegit.php
@@ -1,3 +1,5 @@
 <?php
     echo "Agrego contenido en \"vuelvegit.php\" como flujo de trabajo del main. ";
+
+    echo "Agrego contenido. Desde aca: main, no hay idea de lo que pasa en la rama login.";
 ?>
