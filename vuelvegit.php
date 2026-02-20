f17d0a12e938957f1397775c634687abad04c088
Pilar <pilaralvarez080@gmail.com>
2026-02-19 21:19:42 -0300

    Bienvenida a GitHub

--- vuelvegit.php
+++ vuelvegit.php
@@ -2,4 +2,6 @@
     echo "Agrego contenido en \"vuelvegit.php\" como flujo de trabajo del main. ";
 
     echo "Agrego contenido. Desde aca: main, no hay idea de lo que pasa en la rama login.";
+
+    echo "Bienvenida a GitHub";
 ?>
